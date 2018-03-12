40c49760acd9c4e7b01a86e96dfdb0cd0e502890
Valeria <valeria_g02@abv.bg>
2018-03-12 21:30:28 +0200

    Comments for problem 1 by user 4(ValeriaaG)

--- intro-and-basic-syntax/exercise/DebitCardNumber.java
+++ intro-and-basic-syntax/exercise/DebitCardNumber.java
@@ -10,13 +10,13 @@ import java.io.InputStreamReader;
 public class DebitCardNumber {
     public static void main(String[] args) throws IOException {
 
-        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < 4; i++) {
-            int num = Integer.parseInt(console.readLine());
-            //result.append(String.format("%04d", num)).append(" ");
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));//here we initiate the console
+        StringBuilder result = new StringBuilder();//in this line we make stringbuilder to hold the creditcard numbers
+        for (int i = 0; i < 4; i++) {                      //by this loop we read each number
+            int num = Integer.parseInt(console.readLine()); //we receive 4 integers on the console 
+            result.append(String.format("%04d", num)).append(" ");//we format the result in 4-digit debit card format
         }
 
-        System.out.println(result);
+        System.out.println(result); //printing the result 
     }
 }
