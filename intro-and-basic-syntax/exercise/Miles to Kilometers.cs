bea6d3bb9bf0eba8cf08122faccbc1d07be43c34
XuTkO <xumko@abv.bg>
2018-03-12 21:16:27 +0200

    comments for problem 3 (Miles to Kilometers), from user 2 (XuTkO)

--- intro-and-basic-syntax/exercise/Miles to Kilometers.cs
+++ intro-and-basic-syntax/exercise/Miles to Kilometers.cs
@@ -9,9 +9,16 @@ namespace MilesToKilometers
     class MilesToKilometers
     {
         static void Main(string[] args)
-        {
+        {	 //We recieve from Console
+		   mile values in floating format // 
             double mile = double.Parse(Console.ReadLine());
+
+		 //In this line we calculate how kilometers
+		   corresponds to miles from above line//
             double kilometers = mile * 1.60934;
+
+		//Finally we print the result next to second
+		  floating point.//
             Console.WriteLine($"{kilometers:f2}");
         }
     }
