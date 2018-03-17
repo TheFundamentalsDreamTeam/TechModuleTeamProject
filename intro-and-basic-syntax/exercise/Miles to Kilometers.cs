819fcb1b74e5012473051d8ef42a22537421b1dc
Valeria <valeria_g02@abv.bg>
2018-03-17 21:38:22 +0200

    Member 4 changed a variable name

--- intro-and-basic-syntax/exercise/Miles to Kilometers.cs
+++ intro-and-basic-syntax/exercise/Miles to Kilometers.cs
@@ -11,15 +11,15 @@ namespace MilesToKilometers
         static void Main(string[] args)
         {	 //We recieve from Console
 		   mile values in floating format // 
-            double mile = double.Parse(Console.ReadLine());
+            double miles = double.Parse(Console.ReadLine());
 
 		 //In this line we calculate how kilometers
 		   corresponds to miles from above line//
-            double kilometers = mile * 1.60934;
+            double kmSum = miles * 1.60934;
 
 		//Finally we print the result next to second
 		  floating point.//
-            Console.WriteLine($"{kilometers:f2}");
+            Console.WriteLine($"{kmSum:f2}");
         }
     }
 }
