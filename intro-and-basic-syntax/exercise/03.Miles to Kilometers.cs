c2abd4771b2237eeb78f16df9023ce0bf8c26125
Valeria <valeria_g02@abv.bg>
2018-03-18 17:08:55 +0200

    Adding changes in comments from member 4

--- intro-and-basic-syntax/exercise/03.Miles to Kilometers.cs
+++ intro-and-basic-syntax/exercise/03.Miles to Kilometers.cs
@@ -10,15 +10,15 @@ namespace MilesToKilometers
     {
         static void Main(string[] args)
         {	 //We recieve from Console
-		   mile values in floating format // 
+		   //mile values in floating format
             double miles = double.Parse(Console.ReadLine());
 
 		 //In this line we calculate how kilometers
-		   corresponds to miles from above line//
+		   //corresponds to miles from above line
             double kmSum = miles * 1.60934;
 
 		//Finally we print the result next to second
-		  floating point.//
+		  //floating point.
             Console.WriteLine($"{kmSum:f2}");
         }
     }
