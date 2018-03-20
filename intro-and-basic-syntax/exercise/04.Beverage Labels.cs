b12680b306221ad0aca928daeab5e4b368c2babf
lSaki <a_dimitrov@live.com>
2018-03-20 09:19:15 +0200

    Rebamed variabels in Problem 4

--- intro-and-basic-syntax/exercise/04.Beverage Labels.cs
+++ intro-and-basic-syntax/exercise/04.Beverage Labels.cs
@@ -7,14 +7,14 @@ namespace _04_Beverage_Labels
         static void Main(string[] args)
         {
 			//READ A PRODUCT NAME...
-            var name = Console.ReadLine(); 	//Read product name from Console.
+            var productName = Console.ReadLine(); 	//Read product name from Console.
             int productVolume = int.Parse(Console.ReadLine());		//Read volume of product by “Mililiters” form Console.
-            var energy = int.Parse(Console.ReadLine());		//Read energy of product by “Kcal” from Console.
-            var sugarContent = int.Parse(Console.ReadLine());	//Read sugar of product by “Grams” from Console.
+            var energyOfProduct = int.Parse(Console.ReadLine());		//Read energy of product by “Kcal” from Console.
+            var sugarContentOfProduct = int.Parse(Console.ReadLine());	//Read sugar of product by “Grams” from Console.
 
 			//CALCULATIONS...
-            var kalories = volume * energy / 100.0;		//Calculate “kalories” of product by “Kcal” (energy content per 100ml).
-            var sugar = volume * sugarContent / 100.0;	//Calculate “sugars” of product by “Grams”. (sugar content per 100ml).
+            var kaloriesInKCal = volume * energy / 100.0;		//Calculate “kalories” of product by “Kcal” (energy content per 100ml).
+            var sugarInProduct = volume * sugarContent / 100.0;	//Calculate “sugars” of product by “Grams”. (sugar content per 100ml).
 			
 			//PRINT ON CONSOLE RESULT OF CALCULATIONS...
             Console.WriteLine($"{volume}ml {name}:");	//Print volume of product by “Mililiters” and name of product.
